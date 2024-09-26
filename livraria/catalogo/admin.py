635a4cc49406bbec911bfe72b001a4545a61409a
Gabriel Vinicius <gabrielviniciuslcm@gmail.com>
2024-09-25 22:00:08 -0300

    feat: criado model do carrinho de compras

--- livraria/catalogo/admin.py
+++ livraria/catalogo/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Livro
+from .models import Livro, Carrinho, ItemCarrinho
 
 admin.site.register(Livro)
+admin.site.register(Carrinho)
+admin.site.register(ItemCarrinho)
--- livraria/catalogo/models.py
+++ livraria/catalogo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
@@ -12,3 +13,21 @@ class Livro(models.Model):
 
     def __str__(self):
         return self.titulo
+    
+# Modelo representando um item no carrinho
+class ItemCarrinho(models.Model):
+    livro_id = models.CharField(max_length=255)  # Armazena o ID do livro vindo da API
+    titulo = models.CharField(max_length=255)  # Título do livro
+    quantidade = models.PositiveIntegerField(default=1)  # Quantidade do livro no carrinho
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)  # Relaciona com o usuário
+
+    def __str__(self):
+        return f'{self.quantidade} x {self.titulo}'
+
+# Modelo representando o carrinho do usuário
+class Carrinho(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)  # Cada usuário tem um único carrinho
+    itens = models.ManyToManyField(ItemCarrinho)  # Carrinho tem vários itens
+
+    def __str__(self):
+        return f'Carrinho de {self.usuario.username}'
