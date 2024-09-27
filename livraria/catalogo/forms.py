20955cfd5fb90590b0a0800d3ebe0a3e0e4cf58a
Gabriel Vinicius <gabrielviniciuslcm@gmail.com>
2024-09-27 17:49:42 -0300

    fix: visuais corrigidos para melhor UX

--- livraria/catalogo/forms.py
+++ livraria/catalogo/forms.py
@@ -2,17 +2,22 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Perfil
 
 class RegistroUsuarioForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    nome = forms.CharField(max_length=100, required=True)
+    endereco = forms.CharField(max_length=255, required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2', 'nome', 'endereco']
 
     def save(self, commit=True):
         user = super().save(commit=False)
         user.email = self.cleaned_data['email']
         if commit:
             user.save()
+            perfil = Perfil(user=user,username= self.cleaned_data['username'],  nome=self.cleaned_data['nome'], endereco=self.cleaned_data['endereco'], email=self.cleaned_data['email'])
+            perfil.save()
         return user
--- livraria/catalogo/models.py
+++ livraria/catalogo/models.py
@@ -1,5 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
+
+
+class Perfil(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
